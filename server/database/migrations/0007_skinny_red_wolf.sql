4a7ca1e129edeaebeca06f89bded6fc76edd693c
barbapapazes <e.soubiran25@gmail.com>
2024-05-19 16:13:31 +0200

    feat: add createdAt and updatedAt

---
+++ server/database/migrations/0007_skinny_red_wolf.sql
@@ -0,0 +1,6 @@
+ALTER TABLE categories ADD `created_at` text NOT NULL;--> statement-breakpoint
+ALTER TABLE categories ADD `updated_at` text NOT NULL;--> statement-breakpoint
+ALTER TABLE modules ADD `created_at` text NOT NULL;--> statement-breakpoint
+ALTER TABLE modules ADD `updated_at` text NOT NULL;--> statement-breakpoint
+ALTER TABLE users ADD `created_at` text NOT NULL;--> statement-breakpoint
+ALTER TABLE users ADD `updated_at` text NOT NULL;
--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,25 +1,32 @@
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
+import { sql } from 'drizzle-orm'
 
-export const users = sqliteTable("users", {
-  id: integer("id").primaryKey({ autoIncrement: true }),
-  githubId: integer("github_id").notNull().unique(),
-  email: text("email").notNull().unique(),
-  login: text("login").notNull().unique(),
-  name: text("name"),
-  avatarUrl: text("avatar_url").notNull(),
-  roleType: text("role_type", { enum: ['admin', 'creator'] }).default('creator')
-});
-
-export const categories = sqliteTable("categories", {
-  id: integer("id").primaryKey({ autoIncrement: true }),
-  slug: text("slug").notNull().unique(),
-  name: text("name").notNull(),
+export const users = sqliteTable('users', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  githubId: integer('github_id').notNull().unique(),
+  email: text('email').notNull().unique(),
+  login: text('login').notNull().unique(),
+  name: text('name'),
+  avatarUrl: text('avatar_url').notNull(),
+  roleType: text('role_type', { enum: ['admin', 'creator'] }).default('creator'),
+  createdAt: text('created_at').notNull().$defaultFn(() => sql`(current_timestamp)`),
+  updatedAt: text('updated_at').notNull().$defaultFn(() => sql`(current_timestamp)`).$onUpdateFn(() => sql`(current_timestamp)`),
 })
 
-export const modules = sqliteTable("modules", {
-  id: integer("id").primaryKey({ autoIncrement: true }),
-  name: text("name").notNull().unique(),
-  repo: text("repo").notNull().unique(),
-  type: text("type", { enum: ['official', 'community'] }).notNull(),
-  icon: text("icon"),
+export const categories = sqliteTable('categories', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  slug: text('slug').notNull().unique(),
+  name: text('name').notNull(),
+  createdAt: text('created_at').notNull().$defaultFn(() => sql`(current_timestamp)`),
+  updatedAt: text('updated_at').notNull().$defaultFn(() => sql`(current_timestamp)`).$onUpdateFn(() => sql`(current_timestamp)`),
+})
+
+export const modules = sqliteTable('modules', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  name: text('name').notNull().unique(),
+  repo: text('repo').notNull().unique(),
+  type: text('type', { enum: ['official', 'community'] }).notNull(),
+  icon: text('icon'),
+  createdAt: text('created_at').notNull().$defaultFn(() => sql`(current_timestamp)`),
+  updatedAt: text('updated_at').notNull().$defaultFn(() => sql`(current_timestamp)`).$onUpdateFn(() => sql`(current_timestamp)`),
 })
