85b62a3d9b1fd6fd77fc88d0e6ab6592c3c45719
barbapapazes <e.soubiran25@gmail.com>
2024-05-19 14:26:09 +0200

    refactor(modules): update some variables names

--- server/api/modules/fetch.post.ts
+++ server/api/modules/fetch.post.ts
@@ -5,27 +5,21 @@ interface Modules {
 export default defineEventHandler(async (event) => {
   await requireAdminUser(event)
 
-  const data = await $fetch<Modules>('https://api.nuxt.com/modules', {
-    headers: {
-      'Content-Type': 'application/json'
-    },
-  })
+  const data = await $fetch<Modules>('https://api.nuxt.com/modules')
   const modules = data.modules
     .map(({ name, type, icon, repo }) => ({ name, type, icon, repo }))
     .filter(({ type }) => type === 'official' || type === 'community')
 
-  const keys = Object.keys(modules[0]).length
-
   // A values cannot insert more than 100 bound parameters.
   // So we need to insert in multiple times
+  const columnsPerModule = Object.keys(modules[0]).length
+  const insertPerLoop = Math.floor(100 / columnsPerModule)
+  const loops = Math.ceil(modules.length / insertPerLoop)
 
-  const maximumInsert = Math.floor(100 / keys)
-  const times = Math.ceil(modules.length / maximumInsert)
-
-  for (let i = 0; i < times; i++) {
-    const values = modules.slice(i * maximumInsert, (i + 1) * maximumInsert)
+  for (let loop = 0; loop < loops; loop++) {
+    const values = modules.slice(loop * insertPerLoop, (loop + 1) * insertPerLoop)
     await useDrizzle().insert(tables.modules).values(values)
-    .onConflictDoNothing({ target: tables.modules.repo }).execute()
+      .onConflictDoNothing({ target: tables.modules.repo }).execute()
   }
 
   return sendNoContent(event, 204)
